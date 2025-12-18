6e8f44bc00e68d14fa711477467d3e19ba0791c5
agitkali <agitkali92@gmail.com>
2025-12-19 00:27:01 +0530

    IdentityFrameworkMVC

---
+++ IDentityFramework/ASPNETCoreIdentityFrameWork/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNETCoreIdentityFrameWork.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
